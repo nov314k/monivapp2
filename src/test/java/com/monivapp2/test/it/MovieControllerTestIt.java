6cec35ce819a0960f3f7c3294a9eb850ab89d69b
Novak Petrovic <npetrovic@gmail.com>
2020-02-28 00:28:29 +0100

    Add integration test

--- src/test/java/com/monivapp2/test/it/MovieControllerTestIt.java
+++ src/test/java/com/monivapp2/test/it/MovieControllerTestIt.java
@@ -1,7 +1,10 @@
 package com.monivapp2.test.it;
 
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -11,10 +14,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class MovieControllerTestIt {
 	
+	@Autowired
 	private TestRestTemplate restTemplate;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws JSONException {
 		
+		String response = this.restTemplate.getForObject("/rest/movies",  String.class);
+		JSONAssert.assertEquals("["
+				+ "{id:1,title:\"The Matrix\"},"
+				+ "{id:2,title:\"The Matrix Reloaded\"},"
+				+ "{id:3,title:\"The Matrix Revolutions\"}"
+				+ "]", response, false);
 	}
 }
