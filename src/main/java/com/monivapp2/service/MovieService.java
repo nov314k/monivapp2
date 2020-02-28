0b784ff2803d051c966682ffeabead2da6be3f2d
Novak Petrovic <npetrovic@gmail.com>
2020-02-28 04:55:04 +0100

    Add voteForMovie

--- src/main/java/com/monivapp2/service/MovieService.java
+++ src/main/java/com/monivapp2/service/MovieService.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.monivapp2.entity.Movie;
+import com.monivapp2.entity.Vote;
+import com.monivapp2.exception.MovieNotFoundException;
 import com.monivapp2.repository.MovieRepository;
+import com.monivapp2.repository.VoteRepository;
 
 @Component
 public class MovieService {
@@ -14,13 +17,32 @@ public class MovieService {
 	@Autowired
 	private MovieRepository movieRepository;
 	
+	@Autowired
+	private VoteRepository voteRepository;
+	
 	public List<Movie> getAllMovies() {
 		
 		return movieRepository.findAll();
 	}
-
+	
+	public List<Movie> getAllMoviesOrderByVotesDesc() {
+		
+		return movieRepository.findAllByOrderByVotesDesc();
+	}
+	
 	public Movie addMovie(Movie theMovie) {
 
 		return movieRepository.save(theMovie);
+	}
+
+	public Movie voteForMovie(int movieId) {
+
+		Movie theMovie = movieRepository.findById(movieId)
+				.orElseThrow(() -> new MovieNotFoundException(movieId));
+		theMovie.setVotes(theMovie.getVotes() + 1);
+		movieRepository.save(theMovie);
+		Vote theVote = new Vote(movieId);
+		voteRepository.save(theVote);
+		return theMovie;
 	}
 }
