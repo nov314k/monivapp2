e44bd19a1d66734aa14ca68cd4d31ca83444b780
Novak Petrovic <npetrovic@gmail.com>
2020-02-28 04:54:19 +0100

    Add findAllByOrderByVotesDesc()

--- src/main/java/com/monivapp2/repository/MovieRepository.java
+++ src/main/java/com/monivapp2/repository/MovieRepository.java
@@ -1,9 +1,12 @@
 package com.monivapp2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.monivapp2.entity.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
+	List<Movie> findAllByOrderByVotesDesc();
 }
