63635b5f5000d5e54c328ee2ce77be130647f73d
Novak Petrovic <npetrovic@gmail.com>
2020-02-28 02:19:29 +0100

    Add POST test

--- src/test/java/com/monivapp2/test/rest/MovieRestControllerTest.java
+++ src/test/java/com/monivapp2/test/rest/MovieRestControllerTest.java
@@ -1,23 +1,33 @@
 package com.monivapp2.test.rest;
 
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
 import java.util.Arrays;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.monivapp2.entity.Movie;
 import com.monivapp2.rest.MovieRestController;
 import com.monivapp2.service.MovieService;
@@ -33,7 +43,7 @@ public class MovieRestControllerTest {
 	private MovieService movieService;
 	
 	@Test
-	public void restMovies() throws Exception{
+	public void restGetMovies() throws Exception {
 		
 		when(movieService.getAllMovies()).thenReturn(Arrays.asList(
 						new Movie(1,"The Matrix"),
@@ -50,9 +60,52 @@ public class MovieRestControllerTest {
 				.get("/rest/movies")
 				.accept(MediaType.APPLICATION_JSON);
 		
-		mockMvc.perform(request)
+		MvcResult mvcResult = mockMvc.perform(request)
 				.andExpect(status().isOk())
 				.andExpect(content().json(expected))
 				.andReturn();
+	}
+	
+	@Test
+	public void restPostMovie() throws Exception {
+		
+		String movieToAdd = "{\"title\":\"Movie to Add\"}";
+		
+		RequestBuilder request = MockMvcRequestBuilders
+				.post("/rest/movie")
+				.accept(MediaType.APPLICATION_JSON)
+				.content(movieToAdd)
+				.contentType(MediaType.APPLICATION_JSON);
+		
+	    MvcResult result = mockMvc
+	    		.perform(request)
+	    		.andExpect(status().isOk())
+	    		.andReturn();
+	}
+	
+	@Test
+	public void givenDataIsJson_whenDataIsPostedByPostForObject_thenResponseBodyIsNotNull()
+	  throws IOException, JSONException {
+		
+		RestTemplate restTemplate = new RestTemplate();
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		JSONObject movieJsonObject = new JSONObject();
+	    movieJsonObject.put("id", 100);
+	    movieJsonObject.put("title", "Yet Another Movie");
+	    
+	    final ObjectMapper objectMapper = new ObjectMapper();
+		
+	    HttpEntity<String> request = 
+	      new HttpEntity<String>(movieJsonObject.toString(), headers);
+	     
+	    String movieResultAsJsonStr = 
+	      restTemplate.postForObject("http://localhost:8080/rest/movie/", request, String.class);
+	    JsonNode root = objectMapper.readTree(movieResultAsJsonStr);
+	     
+	    assertNotNull(movieResultAsJsonStr);
+	    assertNotNull(root);
+	    assertNotNull(root.path("title").asText());
 	}
 }
